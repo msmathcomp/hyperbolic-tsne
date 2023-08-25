713a891e6d7d60a1d100e135bb07cc54d147ed7a
Hunter van Geffen <h.c.vangeffen@student.tudelft.nl>
2023-08-25 10:35:17 +0200

    Don't check hidden files with embedding finding functions

--- hyperbolicTSNE/util.py
+++ hyperbolicTSNE/util.py
@@ -5,6 +5,9 @@ import numpy as np
 
 def find_last_embedding(log_path):
     for subdir, dirs, files in reversed(list(os.walk(log_path))):
+        files = [f for f in files if not f[0] == '.']
+        dirs[:] = [d for d in dirs if not d[0] == '.']
+
         for fi, file in enumerate(reversed(sorted(files, key=lambda x: int(x.split(", ")[0])))):
             root, ext = os.path.splitext(file)
             if ext == ".csv":
@@ -16,6 +19,9 @@ def find_last_embedding(log_path):
 def find_ith_embedding(log_path, i):
     j = 0
     for subdir, dirs, files in list(os.walk(log_path)):
+        files = [f for f in files if not f[0] == '.']
+        dirs[:] = [d for d in dirs if not d[0] == '.']
+
         for fi, file in enumerate(sorted(files, key=lambda x: int(x.split(", ")[0]))):
             root, ext = os.path.splitext(file)
             if ext == ".csv":
